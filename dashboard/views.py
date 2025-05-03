ad070dacb62fddf8ae3063edf1436686717fcd7f
Karan <karanost12@gmail.com>
2025-05-03 22:01:57 +0530

    Adding Notification in student Panel

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,12 +4,15 @@ from users.models import User
 from institutions.models import Institution
 from django.contrib.auth.decorators import login_required
 from results.models import TestSummary
+from users.models import Notification
 
 def student_dashboard(request):
 
     testSummary = TestSummary.objects.filter(user = request.user)
+    notifications = Notification.objects.filter(user = request.user)
+    unread_count = notifications.filter(read=False).count()
 
-    return render(request, 'dashboard/student_dashboard.html', {'testSummary': testSummary})
+    return render(request, 'dashboard/student_dashboard.html', {'testSummary': testSummary, 'notifications': notifications, 'unread_count':unread_count})
 
 @login_required
 def institution_dashboard(request):
