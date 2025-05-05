e04c10e5912528d9786aebc3794f5bddaf34eedc
Karan <karanost12@gmail.com>
2025-05-05 17:51:50 +0530

    Adding css in the Registration form

--- users/forms.py
+++ users/forms.py
@@ -31,6 +31,20 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'role', 'profile_image', 'password1', 'password2')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        common_class = 'w-100 form-control p-3 border-primary bg-light'
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs['class'] = common_class
+
+                # # Add CSS classes to fields
+                # self.fields['username'].widget.attrs.update({'class': 'w-100 form-control p-3 border-primary bg-light'})
+                # self.fields['email'].widget.attrs.update({'class': 'w-100 form-control p-3 border-primary bg-light'})
+                # self.fields['role'].widget.attrs.update({'class': 'w-100 form-control p-3 border-primary bg-light'})
+                # self.fields['profile_image'].widget.attrs.update({'class': 'w-100 form-control p-3 border-primary bg-light'})
+                # self.fields['password1'].widget.attrs.update({'class': 'w-100 form-control p-3 border-primary bg-light'})
+                # self.fields['password2'].widget.attrs.update({'class': 'w-100 form-control p-3 border-primary bg-light'})
+
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
